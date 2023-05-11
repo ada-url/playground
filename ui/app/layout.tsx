e85494ea25f631d6bb3b17a205d83055ddd29884
Yagiz Nizipli <yagiz@nizipli.com>
2023-05-11 16:59:26 -0400

    add vercel analytics to ui

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { Toaster } from '@/components/ui/toaster'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   )
